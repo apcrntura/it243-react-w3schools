914448d3e54b14f4850ba3362ba0b3442ad5bda0
Rae Allen Tura <rntura@student.apc.edu.ph>
2026-02-09 06:02:31 +0000

    task 03 updated and deployed to vercel

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello WEBPROG IT2 four three!</h1>
+      <h1>Hello WEBPROG IT2 four three!!</h1>
       <h2>This is my new REACT App!!!!!</h2>
     </div>
   );
